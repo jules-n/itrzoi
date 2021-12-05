bd0b9e3868c059e76a9fd5ee7430904f52aee131
jules <neroba.yulia@gmail.com>
2021-12-06 00:53:41 +0200

    feature/LR-3: WIP controller

---
+++ src/main/java/com/nure/br4/domain/models/Menu.java
@@ -0,0 +1,16 @@
+package com.nure.br4.domain.models;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class Menu {
+    private List<Drink> positions;
+}
